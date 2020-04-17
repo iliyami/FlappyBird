31a595d0795116349d9ff1f7f64a35ab3a2154f3
Mi <Iliya.mi78@gmail.com>
2020-04-17 18:58:13 +0430

    Revert "OOP stuffs"
    This reverts commit 6f2a009f83923bc69003894952f55164d94058fd.

--- Bird.h
+++ Bird.h
@@ -1,6 +1,3 @@
-#ifndef BIRD_H
-#define BIRD_H
-
 class bird
 {
 public:
@@ -9,14 +6,4 @@ public:
     float acc = 0.005;
     float speed = 0;
 
-    void set_bird(int, int);
-
 } bird1;
-
-void bird::set_bird(int X, int Y)
-{
-    x = X;
-    y = Y;
-}
-
-#endif
